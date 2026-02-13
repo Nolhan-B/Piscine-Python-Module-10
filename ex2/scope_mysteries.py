0d3ab43b3cb18d3c3ac4c9005a50088ee3173f9a
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-02-13 13:10:07 +0100

    ex 2 done

--- ex2/scope_mysteries.py
+++ ex2/scope_mysteries.py
@@ -16,9 +16,21 @@ def spell_accumulator(initial_power: int) -> callable:
 	return accumulate
 
 
-# def enchantment_factory(enchantment_type: str) -> callable:
+def enchantment_factory(enchantment_type: str) -> callable:
+	return lambda item: f"{enchantment_type} {item}"
 
-# def memory_vault() -> dict[str, callable]:
+def memory_vault() -> dict[str, callable]:
+	vault = {}
+	def store(key, value) -> None:
+		vault[key] = value
+	def recall(key) -> str:
+		if vault[key]:
+			return vault[key]
+		return "Memory not found"
+	return {
+		'store': store,
+		'recall': recall
+	}
 
 def main() -> None:
 	print("\nTesting mage counter...")
@@ -28,6 +40,11 @@ def main() -> None:
 	print(f" Call 3: {call()}")
 
 	print("\nTesting enchantment factory...")
+	ench_flame = enchantment_factory("Flaming")
+	ench_frozen = enchantment_factory("Frozen")
+
+	print(ench_flame("sword"))
+	print(ench_frozen("Shield"))
 
 
 	# print("\nTesting spell accumulator...")
@@ -36,7 +53,11 @@ def main() -> None:
 	# acc = spell_accumulator(power)
 	# print(f"Power is now : {acc(15)}")
 	# print(f"Power is now : {acc(3)}")
-
+	
+	# print("Testing memory vault...")
+	# mem = memory_vault()
+	# mem["store"]("bagpack", "sword")
+	# print(mem["recall"]("bagpack"))
 
 if __name__ == "__main__":
 	main()
